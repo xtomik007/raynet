23fd82de368266adc6f9fc79bd0e513cc2ccd6b7
xtomik007 <145537942+xtomik007@users.noreply.github.com>
2026-01-17 16:27:53 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'singer-v2.8';
+const CACHE_NAME = 'singer-v2.9';
 const ASSETS = [
   'podpis.html',
   'script.js',
-  'manifest.json'
+  'manifest.json',
+  'icon-192.png'
 ];
 
-// 1. Inštalácia - okamžitá aktivácia
 self.addEventListener('install', (event) => {
   self.skipWaiting();
   event.waitUntil(
@@ -17,10 +17,7 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(clients.claim());
 });
 
-// 2. Obsluha požiadaviek
 self.addEventListener('fetch', (event) => {
-  // POZNÁMKA: Sekcia pre POST (303 redirect) je dočasne vypnutá pre test GET metódy
-  
   event.respondWith(
     caches.match(event.request).then((res) => {
       return res || fetch(event.request);
